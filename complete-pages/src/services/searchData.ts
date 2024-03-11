e675a63d3542a452514b8dcbeaf2edde97d0865c
Adi Aharonov <adi.ahronov@gmail.com>
2024-03-11 13:15:13 +0200

    create a seachData service with initial foodItems

---
+++ complete-pages/src/services/searchData.ts
@@ -0,0 +1,42 @@
+interface FoodItem {
+    _id: string;
+    name: string;
+    displayName: string;
+    price: number;
+  }
+  
+  export const foodItems: FoodItem[] = [
+    { _id: '1', name: 'pizza', displayName: 'Pizza', price: 8.99 },
+    { _id: '2', name: 'burger', displayName: 'Burger', price: 6.99 },
+    { _id: '3', name: 'sushi', displayName: 'Sushi', price: 12.99 },
+    { _id: '4', name: 'pasta', displayName: 'Pasta', price: 7.99 },
+    { _id: '5', name: 'salad', displayName: 'Salad', price: 5.99 },
+    { _id: '6', name: 'taco', displayName: 'Taco', price: 3.99 },
+    { _id: '7', name: 'sandwich', displayName: 'Sandwich', price: 4.99 },
+    { _id: '8', name: 'steak', displayName: 'Steak', price: 15.99 },
+    { _id: '9', name: 'soup', displayName: 'Soup', price: 4.49 },
+    { _id: '10', name: 'curry', displayName: 'Curry', price: 8.49 },
+    { _id: '11', name: 'fish_and_chips', displayName: 'Fish and Chips', price: 9.99 },
+    { _id: '12', name: 'lasagna', displayName: 'Lasagna', price: 11.99 },
+    { _id: '13', name: 'pancakes', displayName: 'Pancakes', price: 4.99 },
+    { _id: '14', name: 'waffles', displayName: 'Waffles', price: 4.99 },
+    { _id: '15', name: 'omelette', displayName: 'Omelette', price: 5.99 },
+    { _id: '16', name: 'fried_rice', displayName: 'Fried Rice', price: 7.99 },
+    { _id: '17', name: 'dumplings', displayName: 'Dumplings', price: 6.49 },
+    { _id: '18', name: 'bbq_ribs', displayName: 'BBQ Ribs', price: 14.99 },
+    { _id: '19', name: 'nachos', displayName: 'Nachos', price: 7.49 },
+    { _id: '20', name: 'falafel', displayName: 'Falafel', price: 6.99 },
+    { _id: '21', name: 'pad_thai', displayName: 'Pad Thai', price: 8.99 },
+    { _id: '22', name: 'bruschetta', displayName: 'Bruschetta', price: 4.99 },
+    { _id: '23', name: 'quiche', displayName: 'Quiche', price: 5.99 },
+    { _id: '24', name: 'frittata', displayName: 'Frittata', price: 6.99 },
+    { _id: '25', name: 'chicken_wings', displayName: 'Chicken Wings', price: 8.99 },
+    { _id: '26', name: 'mozzarella_sticks', displayName: 'Mozzarella Sticks', price: 5.99 },
+    { _id: '27', name: 'calamari', displayName: 'Calamari', price: 9.99 },
+    { _id: '28', name: 'ceviche', displayName: 'Ceviche', price: 12.99 },
+    { _id: '29', name: 'churros', displayName: 'Churros', price: 4.99 },
+    { _id: '30', name: 'tiramisu', displayName: 'Tiramisu', price: 6.99 },
+  ];
+  
+ 
+  
